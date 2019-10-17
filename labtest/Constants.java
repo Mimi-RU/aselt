baa38ffa9f319daede2f987f522d047ac53c6970
Anjuman Ara”git config --global user.email “anjumana0@gmail.com”git config --global user.name Anjuman <anjumana0@gmail.com>
2019-10-17 19:26:13 +0600

    step-5

---
+++ labtest/Constants.java
@@ -0,0 +1,5 @@
+package  labtest;
+public final class Constants{
+    public static final  String StartDialog = "Loading data ...";
+    public  static final String DataFile = "students.txt";
+}
--- labtest/StudentList.java
+++ labtest/StudentList.java
@@ -1,16 +1,20 @@
 package labtest;
 import java.io.*;
-import java.text.*;
-import java.util.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+import static labtest.Constants.*;
+
 public class StudentList {
 
 	public static String LoadData() {
-		System.out.println("Loading data ...");
+		System.out.println(StartDialog);
 		String reader = null;
 		try {
 			BufferedReader stream = new BufferedReader(
 					new InputStreamReader(
-							new FileInputStream("students.txt")));
+							new FileInputStream(DataFile)));
 			reader = stream.readLine();
 		} catch (Exception e) {
 
@@ -18,10 +22,10 @@ public class StudentList {
 		return reader;
 	}
 public static void WriteData ( String[] args){
-	System.out.println("Loading data ...");
+	System.out.println(StartDialog);
 	try {
 		BufferedWriter s = new BufferedWriter(
-				new FileWriter("students.txt", true));
+				new FileWriter(DataFile, true));
 		String t = args[0].substring(1);
 		Date d = new Date();
 		String df = "dd/mm/yyyy-hh:mm:ss a";
